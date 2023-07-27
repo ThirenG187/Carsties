3c379a61f3f12dc7e639affb0fa77e3714048b2e
Thiren Govender <theron619@gmail.com>
2023-07-27 20:39:48 +0200

    At the moment this is running as a Distributed Monolith.
    WIP : Add Asynchronous communication between the services, instead of using Synchronous communication.
    
    * Added Project Dependencies for startup.

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 using System.Text.Json;
 
 namespace SearchService.Data
@@ -9,6 +10,7 @@ namespace SearchService.Data
     {
         public static async Task InitDb(WebApplication app)
         {
+            
             await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
 
             await DB.Index<Item>()
@@ -18,18 +20,16 @@ namespace SearchService.Data
                 .CreateAsync();
 
             var count = await DB.CountAsync<Item>();
+            
+            using var scope = app.Services.CreateScope();
 
-            if (count == 0)
-            {
-                Console.WriteLine("No data - attempting to seed.");
-                var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+            var httpClient = scope.ServiceProvider.GetService<AuctionServiceClient>();
 
-                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var items = await httpClient.GetItemsForSearchDb();
 
-                var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+            Console.WriteLine(items.Count + " returned from the auction service");
 
-                await DB.SaveAsync(items);
-            }
+            if(items.Count > 0) await DB.SaveAsync(items);
         }
     }
 }
