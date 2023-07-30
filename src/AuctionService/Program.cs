304bdf6cde6161d53033e58fa0cdbfc73432eaca
Thiren Govender <theron619@gmail.com>
2023-07-30 02:10:58 +0200

    feature: Added all services into docker.

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -8,63 +8,68 @@ using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 {
-    IdentityModelEventSource.ShowPII = true;
-    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-    builder.Services.AddControllers();
+	IdentityModelEventSource.ShowPII = true;
+	ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+	builder.Services.AddControllers();
 
-    builder.Services.AddDbContext<AuctionDbContext>(opts =>
-    {
-        opts.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-    });
+	builder.Services.AddDbContext<AuctionDbContext>(opts =>
+	{
+		opts.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+	});
 
-    builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+	builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-    builder.Services.AddMassTransit(x =>
-    {
-        x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+	builder.Services.AddMassTransit(x =>
+	{
+		x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
 
-        x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
+		x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
 
-        x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
-        {
-            o.QueryDelay = TimeSpan.FromSeconds(10);
+		x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
+		{
+			o.QueryDelay = TimeSpan.FromSeconds(10);
 
-            o.UsePostgres();
-            o.UseBusOutbox();
-        });
+			o.UsePostgres();
+			o.UseBusOutbox();
+		});
 
-        x.UsingRabbitMq((context, cfg) =>
-        {
-            cfg.ConfigureEndpoints(context);
-        });
-    });
+		x.UsingRabbitMq((context, cfg) =>
+		{
+			cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+			{
+				host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+				host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+			});
+			cfg.ConfigureEndpoints(context);
+		});
+	});
 
-    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-        .AddJwtBearer(options =>
-        {
-            options.Authority = builder.Configuration["IdentityServiceUrl"];
-            options.RequireHttpsMetadata = false;
-            options.TokenValidationParameters.ValidateAudience = false;
-            options.TokenValidationParameters.NameClaimType = "username";
-        });
+	builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+		.AddJwtBearer(options =>
+		{
+			options.Authority = builder.Configuration["IdentityServiceUrl"];
+			options.RequireHttpsMetadata = false;
+			options.TokenValidationParameters.ValidateAudience = false;
+			options.TokenValidationParameters.NameClaimType = "username";
+		});
 }
 
 var app = builder.Build();
 {
-    app.UseAuthentication();
-    app.UseAuthorization();
+	app.UseAuthentication();
+	app.UseAuthorization();
 
-    app.MapControllers();
+	app.MapControllers();
 
 
-    try
-    {
-        DbInitializer.InitDb(app);
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e);
-    }
+	try
+	{
+		DbInitializer.InitDb(app);
+	}
+	catch (Exception e)
+	{
+		Console.WriteLine(e);
+	}
 
-    app.Run();
+	app.Run();
 }
